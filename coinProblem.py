784741a04cf4d3f5f784760e5bbd4dfd7995ebc8
Timo Prange <kyrashelwo@gmail.com>
2014-01-06 15:49:40 +0100

    added presentation pics and improved source code

--- coinProblem.py
+++ coinProblem.py
@@ -1,23 +1,27 @@
 def makeChange(setOfChange,value):
-    print setOfChange
-    D = range(value)
+# D[i] states if we found a way to reach i.
+    D = [0] * (value + 1)
+# 0 is trivial to reach.
     D[0] = 1
-    for i in range(value)[1:]:
-        D[i] = 0
 
-    print D
+# coins[i] states which and how much coins we used to reach i.
+    coins = [[0 for x in range(len(setOfChange))] for x in range(value + 1)]
 
-    for v in range(value)[1:]:
-        print v, D
+# check if we can reach value v
+    for v in range(value + 1)[1:]:
+        #print v, D
+        #print coins
         for x in setOfChange:
             if x <= v:
-                D[v] = D[v] + D[v-x]
-            else:
-                D[v] = 0
-    return D[value-1], D
+                if D[v] == 0:
+                    if D[v-x] != 0:
+                        D[v] = 1
+                        for i in range(len(setOfChange)):
+                            coins[v][i] = coins[v-x][i]
+                        coins[v][setOfChange.index(x)] += 1
+                        break
+    return D[value], coins[value]
 
-
-
-print makeChange([3,7],18)
+print makeChange([6,9,20],44)
 
 
